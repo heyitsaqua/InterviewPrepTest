f95d896bf6293b4b4bd82928033f9dc1eb6b1656
heyitsaqua <offbeattsunami@gmail.com>
2022-10-30 09:30:41 -0400

    project setup/initial commit

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -7,14 +7,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class BrowserUtils {
